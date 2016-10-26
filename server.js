9bde45a02aa24a1e175046b754682108d185a66b
Daniel Arteaga <daarteag@ucsd.edu>
2016-10-26 16:15:58 -0700

    Localhost deployment works

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
+
+
+
+// // make express look in the public directory for assets (css/js/img)
+// app.use(express.static(__dirname + '/public'));
+
+// // set the home page route
+// app.get('/', function(req, res) {
+
+//     // ejs render automatically looks in the views folder
+//     res.render('index');
+// });
+
+// app.listen(port, function() {
+//     console.log('Our app is running on http://localhost:' + port);
+// });
+
+
+
 var express = require('express');
 var app = express();
 
@@ -5,19 +24,18 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
+// Print logs to the console and compress pages we send
+// app.use(express.logger());
+// app.use(express.compress());
 
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '../public'));
+// Return all pages in the /static directory
+// whenever they are requested at '/'
+// e.g., http://localhost:3000/index.html
+// maps to /static/index.html on this machine
+app.use(express.static(__dirname + '/views'));
 
-// set the home page route
-app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('index');
-});
-
+// Start the server
+//var port = process.env.PORT || PORT; // 80 for web, 3000 for development
 app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+	console.log("Node.js server running on port %s", port);
 });
